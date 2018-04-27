4b5d6f42852fc3d0d81eecba0ff29add5eb3d658
Tom Hughes <tomtheengineer@gmail.com>
2018-04-27 10:01:02 -0700

    Use size_t format specifier
    This should work for both 64-bit and 32-bit builds, where size_t is
    different.
    
    Signed-off-by: Tom Hughes <tomtheengineer@gmail.com>

--- modules/imgproc/NE10_boxfilter.c
+++ modules/imgproc/NE10_boxfilter.c
@@ -368,7 +368,7 @@ void ne10_img_boxfilter_rgba8888_c (const ne10_uint8_t *src,
     if (!dst_buf)
     {
         fprintf (stderr,
-                 "ERROR: buffer allocation fails!\nallocation size: %lu\n",
+                 "ERROR: buffer allocation fails!\nallocation size: %zu\n",
                  sizeof (ne10_uint8_t) *
                  src_sz.x *
                  src_sz.y *
